2da7da33032cbb36c47008c374c5102ea31bbb02
vigorouslightning <kjp2562@gmail.com>
2016-03-09 19:50:58 +0000

    Created function for parsing Spell Info

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,5 +1,3 @@
-/*global app*/
-
 app.controller("lolController", ['$scope', '$http', 'dataCache',function($scope, $http, dataCache) {
     $scope.datadragonURL = '';
     
@@ -36,17 +34,16 @@ app.controller("champController", ['$scope', '$http', '$routeParams', '$sce', fu
     $scope.champID = $routeParams.champID;
     $scope.splashURL = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/';
     $scope.spellURL = 'http://ddragon.leagueoflegends.com/cdn/5.18.1/img/spell/';
+    $scope.parsePlaceholders = function(spell) {
+        spell.tooltip = spell.tooltip.replace(/{{ e1 }}/, spell.effect[1]);
+        return spell.tooltip;
+    };
 
     $http.get('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + $scope.champID + '?champData=all&api_key=b322dc6d-0fcf-4484-9fc3-a9a902a32274').success(function(response){
 	    $scope.champInfo = response;
 	    angular.forEach($scope.champInfo.spells, function(val, key) {
-	        //$scope.champInfo.spells[key].tooltip = $scope.champInfo.spells[key].tooltip.replace(/e1/g, "effect[1]");
-	        val.tooltip = val.tooltip.replace(/{{ e1 }}/g, val.effect[1].join("/"))
-	                        .replace(/{{ e2 }}/g, val.effect[2].join("/"))
-	                        .replace(/{{ e3 }}/g, val.effect[3].join("/"))
-	                        //.replace(/{{ e4 }}/g, val.effect[4].join("/"));
-	        console.log(val.tooltip);
-	        console.log("This is $scope" + $scope.champInfo.spells[key].tooltip);
+	        val.tooltip = $scope.parsePlaceholders(val);
+
 	    });
 	});
 }]);
@@ -56,16 +53,3 @@ app.filter('to_trusted', ['$sce', function($sce) {
         return $sce.trustAsHtml(text.replace(/class=\"color/g, "style=\"color: #"));
     };
 }]);
-
-/*
-app.directive("spelltip", function($compile) {
-  return {
-    scope: true,
-    restrict: 'E',
-    template: '<div style=\"border: 1px solid red;\"><div ng-bind-html=\"spell.tooltip | to_trusted\"</div></div>',
-    link: function(scope, element, attrs) {
-        
-    }
-  };
-});
-*/
